b61324134c55c84961bbb828a5516e1dcafb0b0f
Zach <zachlandis91@gmail.com>
2022-12-21 10:17:07 -0700

    commit tree b74f4fcaf16aea4d028c2a665d38051ae473d414

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+
+function superbowlWin(record) {
+    const win = record.find(function (game) {
+        if (game.result === 'W') {
+            return game.year;
+        }
+    })
+    if (win) {
+        return win.year;
+    } else {
+        return undefined;
+    }
+}
